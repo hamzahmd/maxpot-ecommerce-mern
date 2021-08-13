c21fe4dd633b571c703cc1d6f6dec59b2024c093
Hamza Ahmad <hamzahmd.dev@gmail.com>
2021-08-13 12:54:18 +0500

    Frontend intial setup with React and Material-ui

--- Readme.md
+++ Readme.md
@@ -1,1 +1,3 @@
 # Maxpot - MERN ECOMMERCE
+
+MAXPOT (Maximum Potential) is an Ecommerce store built with MERN Stack where you can online purchase Mixed Martial Arts Products.
---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css';
+import { Typography } from '@material-ui/core';
+
+function App() {
+  return <Typography>Hello React World</Typography>;
+}
+
+export default App;
