f63bbd714cafc3050fd37c252c54579002c69849
Hamza Ahmad <hamzahmd.dev@gmail.com>
2021-08-17 12:47:50 +0500

    Implementing Redux: Bringing Products from the Redux State

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,19 +1,22 @@
-import axios from 'axios'
+import axios from 'axios';
 import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
 } from '../reducers/types';
 
-export const ListProducts = ( ) =>async(dispatch) {
+export const listProducts = () => async (dispatch) => {
   try {
-    dispatch({type:PRODUCT_LIST_REQUEST})
-    const {data} = await axios.get('/api/products')
-    dispatch({type:PRODUCT_LIST_SUCCESS,payload:data})
-
+    dispatch({ type: PRODUCT_LIST_REQUEST });
+    const { data } = await axios.get('/api/products');
+    dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
   } catch (error) {
-    dispatch({type:PRODUCT_LIST_FAIL,payload:
-    error.response && error.response.data.message?error.message.data.message:error.message
-    })
+    dispatch({
+      type: PRODUCT_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.message.data.message
+          : error.message,
+    });
   }
-}
+};
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,30 +1,34 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import Product from '../Components/Product';
-import axios from 'axios';
 import { Grid, Typography } from '@material-ui/core';
-
+import { useDispatch, useSelector } from 'react-redux';
+import { listProducts } from '../actions/productActions';
 const HomePage = () => {
-  const [products, setProducts] = useState([]);
-
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
   useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products');
-      setProducts(data);
-    };
-    fetchProducts();
-  }, []);
+    dispatch(listProducts());
+  }, [dispatch]);
+
   return (
     <Fragment>
       <Typography variant='h5' component='h1' style={{ padding: '1.5rem' }}>
         Latest Products
       </Typography>
-      <Grid container spacing={3}>
-        {products.map((product) => (
-          <Grid item key={product._id} xs={12} sm={6} md={4} lg={3} xl={3}>
-            <Product product={product} />
-          </Grid>
-        ))}
-      </Grid>
+      {loading ? (
+        <h2>loading...</h2>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Grid container spacing={3}>
+          {products.map((product) => (
+            <Grid item key={product._id} xs={12} sm={6} md={4} lg={3} xl={3}>
+              <Product product={product} />
+            </Grid>
+          ))}
+        </Grid>
+      )}
     </Fragment>
   );
 };
