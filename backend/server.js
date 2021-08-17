df7c9639b2828d134ff05fa2deaa225c03838d41
Hamza Ahmad <hamzahmd.dev@gmail.com>
2021-08-17 09:52:00 +0500

    Fetching data from the Database using express routes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
-import products from './data/products.js';
+import productRoutes from './routes/productRoutes.js';
 
 dotenv.config();
 connectDB();
@@ -12,14 +12,8 @@ app.get('/', (req, res) => {
   res.send('API is running');
 });
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((prod) => prod._id === req.params.id);
-  res.json(product);
-});
-
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
+// mounting to routes
+app.use('/api/products', productRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
