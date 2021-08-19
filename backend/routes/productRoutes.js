49229f9b7b74d8606228395460bdf1ca76904cb6
Hamza Ahmad <hamzahmd.dev@gmail.com>
2021-08-19 11:53:44 +0500

    Splitting product route into a controller file in the routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,32 +1,8 @@
 import express from 'express';
-import Product from '../models/productModel.js';
-import asyncHandler from 'express-async-handler';
 const router = express.Router();
+import { getProducts, getProductById } from '../routes/productController.js';
 
-// @des     Fetch all products
-// @route   GET /api/products
-// @access  Public
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
-
-// @des     Fetch the selected product
-// @route   GET /api/products/:id
-// @access  Public
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404).json({ message: 'Product not found' });
-    }
-  })
-);
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
 export default router;
