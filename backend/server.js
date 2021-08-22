9d3834412b734efc55be5342fdfd19df269a444d
Hamza Ahmad <hamzahmd.dev@gmail.com>
2021-08-22 10:52:26 +0500

    Order Controller and Routes

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
@@ -16,6 +17,8 @@ app.use(express.json());
 app.use('/api/products', productRoutes);
 
 app.use('/api/users', userRoutes);
+
+app.use('/api/orders', orderRoutes);
 
 // Custom Error Handling
 app.use(notFound);
