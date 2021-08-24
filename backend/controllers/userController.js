7c327dc053895cbb8188995be10ff2687ca9d80c
Hamza Ahmad <hamzahmd.dev@gmail.com>
2021-08-24 14:30:36 +0500

    Get User by Id and Update User Endpoints in the backend for admin

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -119,6 +119,44 @@ const deleteUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @des     GET userby ID
+// @route   GET /api/users/:id
+// @access  Private/Admin
+const getUsersById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select('-password');
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+// @des     Update user
+// @route   PUT /api/users/:id
+// @access  Private/Admin
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.isAdmin = req.body.isAdmin;
+
+    const updateUser = await user.save();
+
+    const updateUser = await user.save();
+    res.json({
+      _id: updateUser._id,
+      name: updateUser.name,
+      email: updateUser.email,
+      isAdmin: updateUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
 export {
   authUser,
   getUserProfile,
@@ -126,4 +164,6 @@ export {
   updateUserProfile,
   getUsers,
   deleteUser,
+  getUsersById,
+  updateUser,
 };
