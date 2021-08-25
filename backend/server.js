7a78a04ce461e0e388d674b4fb15ef6580d91137
Hamza Ahmad <hamzahmd.dev@gmail.com>
2021-08-25 20:37:28 +0500

    Image Upload Config and Endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
@@ -19,6 +21,11 @@ app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 
 app.use('/api/orders', orderRoutes);
+
+app.use('/api/upload', uploadRoutes);
+
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 // Custom Error Handling
 app.use(notFound);
