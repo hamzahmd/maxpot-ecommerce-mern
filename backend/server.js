3409a33520e1dc19bba58b68aec77e3cfd571bed
Hamza Ahmad <hamzahmd.dev@gmail.com>
2021-08-26 22:29:27 +0500

    Preparing for Deployment

--- backend/server.js
+++ backend/server.js
@@ -32,6 +32,17 @@ app.use('/api/upload', uploadRoutes);
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running');
+  });
+}
+
 // Custom Error Handling
 app.use(notFound);
 app.use(errorHandler);
